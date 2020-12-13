e99cd0de9717b172cdb9c6055e39afcd8e3519f6
Cender12 <enderchristopher@gmail.com>
2020-12-13 16:13:22 -0500

    set up ejs-mate and layout

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const ejsMate = require('ejs-mate')
 const methodOverride = require('method-override');
 const Road = require('./models/road');
 
@@ -18,13 +19,15 @@ db.once("open", () => {
 
 const app = express();
 
+app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
-//ROUTES
+
+//ROUTES===================================================================================
 app.get('/', (req, res) => {
     res.render('home');
 });
@@ -68,7 +71,7 @@ app.delete('/Roads/:id', async (req, res) => {
 
 
 
-//PORT LISTENER
+//PORT LISTENER============================================================================
 app.listen(3000, () =>{
     console.log('Serving on port 3000...');
 })
