33625805a35c7e969d59481770a2346e56553839
Cender12 <enderchristopher@gmail.com>
2021-01-01 16:45:29 -0500

    create routes/roads.js to restructure

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ const Road = require('./models/road');
 // const road = require('./models/road');
 const Review = require('./models/review');
 
+const roads = require('./routes/roads');
+
+
 mongoose.connect('mongodb://localhost:27017/all-roads',{
     useNewUrlParser:true,
     useCreateIndex: true,
@@ -32,15 +35,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
 // MIDDLEWARE==============================================================================
-const validateRoad = (req, res, next) => {
-    const { error } = RoadSchema.validate(req.body);
-    if(error){
-        const msg = error.details.map(el => el.message).join(',')
-        throw new ExpressError(msg, 400)
-    } else {
-        next();
-    }
-}
+
 
 const validateReview = (req, res, next) => {
     const {error} = reviewSchema.validate(req.body);
@@ -53,48 +48,8 @@ const validateReview = (req, res, next) => {
     }
 }
 
-//ROUTES===================================================================================
-app.get('/', (req, res) => {
-    res.render('home');
-});
 
-app.get('/Roads', catchAsync(async (req, res) => {
-   const roadCollection = await Road.find({});
-   res.render('roads/index', { roadCollection })
-}));
-
-app.get('/Roads/new', (req, res) => {
-    res.render('roads/new')
-});
-
-app.post('/Roads', validateRoad, catchAsync(async(req, res, next) => {
-        // if(!req.body.road) throw new ExpressError('Invalid Road Data', 400);
-        const road = new Road (req.body.road);
-        await road.save();
-        res.redirect(`/Roads/${road._id}`) 
-}));
-
-app.get('/Roads/:id', catchAsync(async (req, res) => {
-    const road = await (await Road.findById(req.params.id)).populate('reviews');
-    res.render('roads/show', { road });
-}));
-
-app.get('/Roads/:id/edit', catchAsync(async (req, res) => {
-    const road = await Road.findById(req.params.id)
-    res.render('roads/edit', { road });
-}));
-
-app.put('/Roads/:id', validateRoad, catchAsync(async (req, res) => {
-    const { id } = req.params;
-    const road = await Road.findByIdAndUpdate(id, { ...req.body.road })
-    res.redirect(`/Roads/${road._id}`)
-}));
-
-app.delete('/Roads/:id', catchAsync(async (req, res) => {
-    const { id } = req.params;
-    await Road.findByIdAndDelete(id);
-    res.redirect('/Roads');
-}));
+app.use('/Roads', roads)
 
 app.post('/Roads/:id/reviews', validateReview, catchAsync(async (req, res) => {
     const road = await Road.findById(req.params.id);
---
+++ routes/roads.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const router = express.Router();
+const catchAsync = require('../utilities/catchAsync');
+const { RoadSchema } = require('../schemas.js');
+const ExpressError = require('../utilities/ExpressError');
+const Road = require('../models/road');
+
+//MIDDLEWARE===============================================================
+const validateRoad = (req, res, next) => {
+    const { error } = RoadSchema.validate(req.body);
+    if(error){
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg, 400)
+    } else {
+        next();
+    }
+}
+
+//ROUTES===============================================================
+router.get('/', catchAsync(async (req, res) => {
+   const roadCollection = await Road.find({});
+   res.render('roads/index', { roadCollection })
+}));
+
+router.get('/new', (req, res) => {
+    res.render('roads/new')
+});
+
+router.post('/', validateRoad, catchAsync(async(req, res, next) => {
+        // if(!req.body.road) throw new ExpressError('Invalid Road Data', 400);
+        const road = new Road (req.body.road);
+        await road.save();
+        res.redirect(`/Roads/${road._id}`) 
+}));
+
+router.get('/:id', catchAsync(async (req, res) => {
+    const road = await (await Road.findById(req.params.id)).populate('reviews');
+    res.render('roads/show', { road });
+}));
+
+router.get('/:id/edit', catchAsync(async (req, res) => {
+    const road = await Road.findById(req.params.id)
+    res.render('roads/edit', { road });
+}));
+
+router.put('/:id', validateRoad, catchAsync(async (req, res) => {
+    const { id } = req.params;
+    const road = await Road.findByIdAndUpdate(id, { ...req.body.road })
+    res.redirect(`/Roads/${road._id}`)
+}));
+
+router.delete('/:id', catchAsync(async (req, res) => {
+    const { id } = req.params;
+    await Road.findByIdAndDelete(id);
+    res.redirect('/Roads');
+}));
+
+module.exports = router;
