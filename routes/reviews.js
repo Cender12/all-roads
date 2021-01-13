5fc6be22f698b9b7a2b56056d586aa61dde3d85d
Cender12 <enderchristopher@gmail.com>
2021-01-13 13:18:44 -0500

    add delete button and delete route for reviews

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router({ mergeParams: true });
-const { validateReview } = require('../middleware');
+const { validateReview, isLoggedIn } = require('../middleware');
 const Road = require('../models/road');
 const Review = require('../models/review');
 const ExpressError = require('../utilities/ExpressError');
@@ -8,9 +8,10 @@ const catchAsync = require('../utilities/catchAsync');
 
 
 
-router.post('/', validateReview, catchAsync(async (req, res) => {
+router.post('/', isLoggedIn, validateReview, catchAsync(async (req, res) => {
     const road = await Road.findById(req.params.id);
     const review = new Review(req.body.review);
+    review.author = req.user_id;
     road.reviews.push(review);
     await review.save();
     await road.save();
@@ -18,12 +19,20 @@ router.post('/', validateReview, catchAsync(async (req, res) => {
     res.redirect(`/Roads/${road._id}`);
 }));
 
-// app.delete('/reviewId', catchAsync(async (req, res) => {
+router.delete('/:reviewId', catchAsync(async (req, res) => {
+    const { id, reviewId} = req.params;
+    await Road.findByIdAndUpdate(id, {$pull: { reviews: reviewId } });
+    await Review.findByIdAndDelete(reviewId);
+    req.flash('success', 'Review deleted')
+    res.redirect(`/Roads/${id}`);
+}));
+
+// router.delete('/reviewId', catchAsync(async (req, res) => {
 //     const { id, reviewId } = req.params;
 //     await Road.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
 //     await Review.findByIdAndDelete(reviewId);
-//     req.flash('success', Review Deleted');
-//     res.redirect(`/Roads/${id}');
+//     req.flash('success', 'Review Deleted');
+//     res.redirect(`/Roads/${id}`);
 // }))
 
 module.exports = router;
