2e027882039b8faf3f5d2566fa1e38783b637d6c
Cender12 <enderchristopher@gmail.com>
2021-02-11 16:24:03 -0500

    change deploy port

--- app.js
+++ app.js
@@ -187,7 +187,9 @@ app.use((err, req, res, next) => {
 });
 
 
+
 // PORT LISTENER============================================================================
-app.listen(3000, () => {
-    console.log('Serving on port 3000...')
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Serving on port ${port}...`)
 });
